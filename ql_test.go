03641d6b5bc3b82bebea7f771f707a96fc71c7fd
gernest <geofreyernest@live.com>
2017-03-29 01:25:30 +0300

    Add tests

--- ql_test.go
+++ ql_test.go
@@ -2,9 +2,17 @@ package ql
 
 import (
 	"database/sql"
+	"reflect"
 	"testing"
 
+	"time"
+
+	"math/big"
+
 	_ "github.com/cznic/ql/driver"
+	"github.com/ngorm/ngorm/engine"
+	"github.com/ngorm/ngorm/model"
+	"github.com/ngorm/ngorm/scope"
 )
 
 type Department struct {
@@ -23,13 +31,13 @@ COMMIT;
 `
 
 func TestDialect(t *testing.T) {
-	dialect := Memory()
-	if dialect.GetName() != "ql-mem" {
-		t.Errorf("expected ql-mem got %s", dialect.GetName())
+	d := Memory()
+	if d.GetName() != "ql-mem" {
+		t.Errorf("expected ql-mem got %s", d.GetName())
 	}
-	dialect = File()
-	if dialect.GetName() != "ql" {
-		t.Errorf("expected ql got %s", dialect.GetName())
+	d = File()
+	if d.GetName() != "ql" {
+		t.Errorf("expected ql got %s", d.GetName())
 	}
 	db, err := sql.Open("ql-mem", "test.db")
 	if err != nil {
@@ -44,33 +52,33 @@ func TestDialect(t *testing.T) {
 	}
 	_, _ = tx.Exec(migration)
 	_ = tx.Commit()
-	dialect.SetDB(db)
-	if dialect.db == nil {
+	d.SetDB(db)
+	if d.db == nil {
 		t.Fatal("expected the database to be set")
 	}
 
 	//
 	// HasIndex
 	//
-	if !dialect.HasIndex("Orders", "OrdersID") {
+	if !d.HasIndex("Orders", "OrdersID") {
 		t.Error("expected to be true")
 	}
 
 	//RemoveIndex
-	err = dialect.RemoveIndex("Orders", "OrdersID")
+	err = d.RemoveIndex("Orders", "OrdersID")
 	if err != nil {
 		t.Error(err)
 	}
 
-	if dialect.HasIndex("Orders", "OrdersID") {
+	if d.HasIndex("Orders", "OrdersID") {
 		t.Error("expected to be fasle")
 	}
 
 	// Has Table
-	if !dialect.HasTable("Orders") {
+	if !d.HasTable("Orders") {
 		t.Error("expected to be true")
 	}
-	if !dialect.HasColumn("Orders", "Date") {
+	if !d.HasColumn("Orders", "Date") {
 		t.Error("expected to be true")
 	}
 
@@ -93,5 +101,75 @@ func TestQL_BindVar(t *testing.T) {
 	v := q.BindVar(src)
 	if v != expect {
 		t.Errorf("expected %s got %s", expect, v)
+	}
+}
+
+type Sample struct {
+	ID        int64
+	CreatedAt time.Time
+	Big       big.Int
+	Rat       big.Rat
+	Blob      []byte
+}
+
+func TestQL_DataTypeOf(t *testing.T) {
+	e := &engine.Engine{
+		Search:    &model.Search{},
+		Scope:     &model.Scope{},
+		StructMap: model.NewStructsMap(),
+	}
+	m, err := scope.GetModelStruct(e, &Sample{})
+	if err != nil {
+		t.Error(err)
+	}
+	q := &QL{}
+	for _, f := range m.StructFields {
+		switch f.Name {
+		case "ID":
+			e := reflect.Int64.String()
+			s, err := q.DataTypeOf(f)
+			if err != nil {
+				t.Fatal(err)
+			}
+			if s != e {
+				t.Errorf("expected %s got %s", e, s)
+			}
+		case "CreatedAt":
+			e := "time"
+			s, err := q.DataTypeOf(f)
+			if err != nil {
+				t.Fatal(err)
+			}
+			if s != e {
+				t.Errorf("expected %s got %s", e, s)
+			}
+		case "Big":
+			e := "bigint"
+			s, err := q.DataTypeOf(f)
+			if err != nil {
+				t.Fatal(err)
+			}
+			if s != e {
+				t.Errorf("expected %s got %s", e, s)
+			}
+		case "Rat":
+			e := "bigrat"
+			s, err := q.DataTypeOf(f)
+			if err != nil {
+				t.Fatal(err)
+			}
+			if s != e {
+				t.Errorf("expected %s got %s", e, s)
+			}
+		case "Blob":
+			e := "blob"
+			s, err := q.DataTypeOf(f)
+			if err != nil {
+				t.Fatal(err)
+			}
+			if s != e {
+				t.Errorf("expected %s got %s", e, s)
+			}
+		}
 	}
 }
